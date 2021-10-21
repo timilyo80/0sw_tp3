d9f080df8d4450c3e49b066fafd8aacc611b99a0
timilyo80 <78914833+timilyo80@users.noreply.github.com>
2021-10-21 12:51:41 -0400

    Code : done

--- player.cs
+++ player.cs
@@ -19,11 +19,15 @@ public class player : KinematicBody2D
 
 	Sprite currentSprite;
 	AnimationPlayer animPlayer;
+	AnimationTree animTree;
+	AnimationNodeStateMachinePlayback animState;
 		// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		currentSprite = GetNode<Sprite>("Sprite");
 		animPlayer = GetNode<AnimationPlayer>("AnimationPlayer");
+		animTree = GetNode<AnimationTree>("AnimationTree");
+		animState = (AnimationNodeStateMachinePlayback)animTree.Get("parameters/playback");
 	}
 
 	public override void _PhysicsProcess(float delta)
@@ -45,11 +49,14 @@ public class player : KinematicBody2D
 		if (Input.IsActionPressed("ui_left")) {
 			motion.x -= ACCEL;
 			facing_right = false;
+			animState.Travel("Run");
 		} else if (Input.IsActionPressed("ui_right")) {
 			motion.x += ACCEL;
 			facing_right = true;
+			animState.Travel("Run");
 		} else {
 			motion = motion.LinearInterpolate(Vector2.Zero, 0.2f);
+			animState.Travel("Idle");
 		}
 
 		if (IsOnFloor())
@@ -59,8 +66,20 @@ public class player : KinematicBody2D
 				GD.Print($"motion.y = {motion.y}");
 				Console.WriteLine($"motion.y = {motion.y}");
 			}
+			
+		if (!IsOnFloor()) {
+			if (motion.y < 0) {
+				animState.Travel("jump");
+			} else if (motion.y > 0) {
+				animState.Travel("fall");
+			}
+		}
 
 		motion = MoveAndSlide(motion, UP);
+		
+		if (Input.IsActionJustPressed("ui_attack")){
+			animState.Travel("attack");
+		}
 	}
 
 
